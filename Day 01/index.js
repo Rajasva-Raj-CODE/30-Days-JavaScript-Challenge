669c1d6cea99f0e53e893f3d7121bee5317927de
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2024-08-30 19:32:00 +0530

    day1 questions

---
+++ Day 01/index.js
@@ -0,0 +1,55 @@
+//Task_1 - Declare a variable using 'var', assign it a number and log the value to the console.
+
+var hero = 123;
+console.log(hero);
+
+//Task_2 - Declare a variable using 'let', assign it a string and log the value to the console.
+ let zero = "rajasva";
+ console.log(zero);
+
+ //Task_3 - Declare a variable using 'let', assign it a boolean value and log the value to the console.
+ let num = true;
+ console.log(num);
+
+//   Create variables of different data types(number, string, boolean , object, array) and log each variable's type using the typeof
+ const numb = 123;
+ console.log(numb ,typeof(numb));
+
+ const str ="Raj"
+ console.log(str,typeof(str));
+
+ const bool = false;
+ console.log(bool,typeof(bool));
+
+ const obj = {
+    action:"MARVEL",
+    SIFI:"Fast and Ferious",
+ }
+ console.log(obj,typeof(obj));
+
+ const arr =[123,"aassaa","!@#$"]
+ console.log(arr,typeof(arr));
+ 
+
+//Task_5 - Declare a variable using 'let', assign it an initial value, reassign a new value and log both the values to console.
+
+let demo = "abc"
+demo="xyz"
+console.log(demo);
+
+//Task_6 - Try reassigning a variable declared with 'const' and observe the error
+
+const demo1 = "jai ho"
+demo1= "jai hind"
+console.log(demo1);
+
+// is will throw error bcoz const can not be re declare or re initial
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
