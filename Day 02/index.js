133f153dbbd791fab0171aa668bf109fd76136c1
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2024-08-31 10:19:34 +0530

    Day 02 question

---
+++ Day 02/index.js
@@ -0,0 +1,110 @@
+//! Task-1 : Write a program to add two numbers and log the result to the console.
+
+const addition1 = 10;
+const addition2 = 20;
+
+const add = addition1 + addition2;
+console.log(add);
+
+//! Task-2 : Write a program to subtract two numbers and log the result to the console.
+
+const subtract1 = 40;
+const subtract2 = 20;
+
+const sub = subtract1 - subtract2;
+console.log(sub);
+
+//! Task-3 : Write a program to multiply two numbers and log the result to the console.
+
+const multiply1 = 7;
+const multiply2 = 8;
+
+const mul = multiply1 * multiply2;
+console.log(mul);
+
+//! Task-4 : Write a program to divide two numbers and log the result to the console.
+
+const divide1 = 60;
+const divide2 = 20;
+
+const div = divide1 / divide2;
+console.log(div);
+
+//! Task-5 : Write a program to find the remainder when one divided by another and log the result to the console.
+
+const rim1 = 12;
+const rim2 = 67;
+
+const remainder = rim1 % rim2;
+console.log(remainder);
+
+//! Task-6 : Use the '+=' operator to add a number to the variable and log the result to the console.
+
+let num1 = 22;
+num1 += 22;
+console.log(num1);
+
+//! Task-7 : Use the '-=' operator to sub a number to the variable and log the result to the console.
+let num2 = 44;
+num2 -= 10;
+console.log(num2);
+
+//! Task-8 : Write a program to compare two numbers using '>' and '<' and log the result to console.
+
+let comp1 = 89;
+let comp2 = 54;
+
+let comparing1 = comp1 > comp2;
+let comparing2 = comp1 < comp2;
+console.log(comparing1);
+console.log(comparing2);
+
+//! Task-9 : Write a program to compare two numbers using '>=' and '<=' and log the result to console.
+
+let compare1 = 34;
+let compare2 = 56;
+
+let compa1 = compare1 >= compare2;
+let compa2 = compare1 <= compare2;
+
+console.log(compa1);
+console.log(compa2);
+
+console.log("-------------");
+
+//! Task-10 : Write a program to compare two numbers using '==' and '===' and log the result to console.
+
+let nub1 = "345";
+let nub2 = 345;
+
+let numb1 = nub1 == nub2;
+let numb2 = nub1 === nub2;
+
+console.log(numb1);
+console.log(numb2);
+
+//! Task-11 : Write a program that used the '&&' operator to combine two conditions and log the result to the console.
+
+let nd1 = 231;
+let nd2 = 456;
+
+console.log(`${nd1 == 231 && nd2 <= 456} `);
+
+//! Task-12 : Write a program that used the '||' operator to combine two conditions and log the result to the console.
+let or1 = 231;
+let or2 = 893;
+
+console.log(`${or1 > 843 || or2 < 700}`);
+
+//! Task-13 : Write a program that used the '!' operator to negate a  condition and log the result to the console.
+let nogt1 = 932;
+let negate = !nogt1;
+
+console.log(nogt1);
+console.log(negate);
+
+//! Task-14 : Write a program that used the ternary operator to check if a number is positive or negative and log the result to the console.
+
+let varname = -33;
+let check = varname > 0 ? "positive" : "negative";
+console.log(check);
