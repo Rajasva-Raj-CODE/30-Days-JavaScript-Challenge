2897a8df93c56362d6355cfe497bbf2b850f5e29
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2024-09-02 23:52:17 +0530

    Day 04 question

---
+++ Day 04/index.js
@@ -0,0 +1,44 @@
+//! Task-1 : Write a program to print numbers from 1 to 10 using a for loop.
+// for (i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+//! Task-2 : Write a program to print the multiplication table of 5 using a for loop.
+// for (i = 5; i < 51; i = i + 5) {
+//   console.log(i);
+// }
+//! Task-3 : Write a program to calculate the sum of numbers form 1 to 10 using a while loop.
+// i = 1;
+// result = 0;
+// while (i <= 10) {
+//   result = result + i;
+//   i++;
+//   console.log(result);
+// }
+//! Task-4 : Write a program to print numbers from 10 to 1 using a while loop.
+// i=11;
+// while(i>=1){
+//     i--
+//     console.log(i);
+    
+// }
+//! Task-5 : Write a program to print numbers from 1 to 5 using do...while loop.
+// let i=1
+// do{
+//     console.log(i);
+//     i++
+// }while(i<=5)
+
+//! Task-6 : Write a program to calculate the factorial of a number using a do...while loop.
+//  var num =5
+//  var fact=1
+//  for(i=num;i>=1;i--)
+//  {
+//     fact =fact*i
+//  }
+//  console.log(fact);
+ 
+
+//!  Task-7 : Write a program to print pattern using nested for loops
+
+//!  Task-8 : Write a program to print numbers form 1 to 10, but skip the number 5 using the 'continue' statement.
+//! Task-9 : Write a program to print numbers from 1 to 10, but stop the loop when the number is 7 using the break statement.
