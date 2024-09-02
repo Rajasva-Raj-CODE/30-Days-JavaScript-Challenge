19ceec6cc8ad48d8d257623ea076bdfb2c8163f0
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2024-09-03 00:31:18 +0530

    Day 04 question done

--- Day 04/index.js
+++ Day 04/index.js
@@ -19,7 +19,7 @@
 // while(i>=1){
 //     i--
 //     console.log(i);
-    
+
 // }
 //! Task-5 : Write a program to print numbers from 1 to 5 using do...while loop.
 // let i=1
@@ -36,9 +36,29 @@
 //     fact =fact*i
 //  }
 //  console.log(fact);
- 
 
 //!  Task-7 : Write a program to print pattern using nested for loops
+// for(i=1;i<=5;i++){
+//     for(j=1;j<=i;j++){
+//         console.log("*");  // not solved!!!!!!!!
+        
+//     }
+    
+// }
 
 //!  Task-8 : Write a program to print numbers form 1 to 10, but skip the number 5 using the 'continue' statement.
+// for (i = 1; i <= 10; i++) {
+//   if (i == 5) {
+//     continue;
+//   }
+//   console.log(i);
+  
+// }
 //! Task-9 : Write a program to print numbers from 1 to 10, but stop the loop when the number is 7 using the break statement.
+// for(i=1;i<=10;i++){
+//     if(i==7){
+//         break
+//     }
+//     console.log();
+    
+// }
